420f26eba96fd10bc2940437af8c707d34fbd793
Arnar Johannsson <arnar.johannsson@qlok.se>
2024-03-13 09:51:07 +0100

    added list and styling

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-  return <main>Content</main>;
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Ett kilo äpplen",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "kakor",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Kalles Kaviar",
+    },
+  ]);
+
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" />
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0" aria-label="delete item" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 };
 
 export default Content;
